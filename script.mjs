27e5147e95fee853a98f81e4f961df6ed1bbf7a8
Fithi Teklom <fthiteklom@gmail.com>
2026-06-09 16:10:46 +0100

    add message that display agenda not found functionality

--- script.mjs
+++ script.mjs
@@ -1,8 +1,27 @@
+import { getData } from "./storage.mjs";
 
 window.addEventListener("DOMContentLoaded", () => {
+
 const dateInput = document.querySelector("#start-date");
 const today = new Date();
 const formattedDate = today.toISOString().split("T")[0];
 
+const userSelect = document.querySelector("#user-select");
+const agendaContainer = document.querySelector("#agenda-container");
+
 dateInput.value = formattedDate;
+
+userSelect.addEventListener("change", () =>{
+    const userId = userSelect.value;
+
+    if (!userId) {
+        agendaContainer.textContent="";
+        return;
+    }
+    const agenda = getData(userId);
+
+    if(!agenda || agenda.length === 0){
+        agendaContainer.textContent ="No agenda found for this user."
+    }
+})
 });
