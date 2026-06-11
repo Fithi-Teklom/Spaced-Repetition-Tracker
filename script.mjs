011307f4682bd18aa4c583d19efdf36a32875c01
Fithi Teklom <fthiteklom@gmail.com>
2026-06-11 13:46:59 +0100

    message appears with data and disappears with no data.

--- script.mjs
+++ script.mjs
@@ -1,65 +1,51 @@
-import { getData , addData } from "./storage.mjs";
-import { calculateRevisionDates} from "./common.mjs";
-
-
+import { getData, addData } from "./storage.mjs";
+import { calculateRevisionDates } from "./common.mjs";
 
 window.addEventListener("DOMContentLoaded", () => {
+  const dateInput = document.querySelector("#start-date");
+  const today = new Date();
+  const formattedDate = today.toISOString().split("T")[0];
 
-const dateInput = document.querySelector("#start-date");
-const today = new Date();
-const formattedDate = today.toISOString().split("T")[0];
+  const userSelect = document.querySelector("#user-select");
+  const agendaContainer = document.querySelector("#agenda-container");
 
-const userSelect = document.querySelector("#user-select");
-const agendaContainer = document.querySelector("#agenda-container");
+  const form = document.querySelector("#topic-form");
+  const topicInput = document.querySelector("#topic");
 
-const form = document.querySelector("#topic-form");
-const topicInput = document.querySelector("#topic");
+  dateInput.value = formattedDate;
 
-dateInput.value = formattedDate;
+  function renderAgenda(userId) {
+    const agenda = getData(userId);
+    
+    if (!agenda || agenda.length === 0) {
+      agendaContainer.textContent = "No agenda found for this user.";
+      return;
+    }
+    agendaContainer.innerHTML ="";
+  }
 
-userSelect.addEventListener("change", () =>{
+  userSelect.addEventListener("change", () => {
     const userId = userSelect.value;
 
     if (!userId) {
-        agendaContainer.textContent="";
-        return;
+      agendaContainer.textContent = "";
+      return;
     }
-    const agenda = getData(userId);
-    console.log(agenda);
+    renderAgenda(userId);
+  });
 
-    if(!agenda || agenda.length === 0){
-        agendaContainer.textContent ="No agenda found for this user."
-    }
-})
-
-form.addEventListener("submit", (submitEvent) => {
+  form.addEventListener("submit", (submitEvent) => {
     submitEvent.preventDefault();
 
     const userId = userSelect.value;
     const topic = topicInput.value;
-     const startDate = dateInput.value;
-   
-    
+    const startDate = dateInput.value;
+
     if (!userId) {
-  alert("Please select a user.");
-  return;
-}
- const revisionDates = calculateRevisionDates(startDate);
- const topicData = {topic, revisionDates};
-   addData(userId, [topicData]);
-   console.log(getData(userId));
-      
-   
-   
-});
-    
-    
-    
-
-
-   
-
-
-
-
+      alert("Please select a user.");
+      return;
+    }
+    const revisionDates = calculateRevisionDates(startDate);
+    const topicData = { topic, revisionDates };
+  });
 });
